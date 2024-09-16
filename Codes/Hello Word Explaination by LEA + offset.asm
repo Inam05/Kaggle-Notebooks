821a72d2d56870ef2657f14819ce4862db51b2d8
Inam05 <inamulhassan.tech@gmail.com>
2024-09-16 09:56:47 +0500

    Hello world expalination by LEA and offset methods

---
+++ Codes/Hello Word Explaination by LEA + offset.asm
@@ -0,0 +1,17 @@
+     
+org 100h       
+
+.data              
+
+msg db "123.556!!!!!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv$"   
+
+
+.code              
+    
+
+    mov dx, offset msg 
+    
+    mov ah, 9    
+    int 21h        
+                    
+      ret
